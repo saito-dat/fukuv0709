6fd60815ecb60eae9e6a7f99aa811d33d201b23f
Fukata <satoshidat@gmail.com>
2020-07-09 09:24:38 +0900

    完成

--- fukuv0709/Form1.cs
+++ fukuv0709/Form1.cs
@@ -16,5 +16,25 @@ namespace fukuv0709
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Top = label1.Top - 10;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Left = label1.Left - 10;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label1.Left = label1.Left + 10;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            label1.Top = label1.Top + 10;
+        }
     }
 }
